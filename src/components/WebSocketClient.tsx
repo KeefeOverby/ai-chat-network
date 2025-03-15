3dd55930635cba3674281b14cfc57c966f05cac9
Keefe Overby <wrappedusername@gmail.com>
2025-03-14 21:36:58 -0500

    configuring files

---
+++ src/components/WebSocketClient.tsx
@@ -0,0 +1,27 @@
+// src/components/WebSocketClient.tsx
+'use client';
+
+import React, { useEffect } from 'react';
+
+interface WebSocketClientProps {
+  setMessages: React.Dispatch<React.SetStateAction<{ content: string; mode: string }[]>>;
+}
+
+const WebSocketClient: React.FC<WebSocketClientProps> = ({ setMessages }) => {
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:3000');
+
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, [setMessages]);
+
+  return null;
+};
+
+export default WebSocketClient;
